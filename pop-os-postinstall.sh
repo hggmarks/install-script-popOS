c8ec53199b03117de5e1bbe94e86c1505250ad2b
hggmarks <hggmarks@gmail.com>
2022-07-14 01:01:18 -0300

    changed packages and added neovim instalation

--- pop-os-postinstall.sh
+++ pop-os-postinstall.sh
@@ -14,11 +14,12 @@
 
 ##URLS
 
-URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.0-1_amd64.deb?source=website"
-URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.7.2.50318-impish_amd64.deb"
-URL_SYNOLOGY_DRIVE="https://global.download.synology.com/download/Utility/SynologyDriveClient/3.0.3-12689/Ubuntu/Installer/x86_64/synology-drive-client-12689.x86_64.deb"
-
+##URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+##URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.0-1_amd64.deb?source=website"
+##URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.7.2.50318-impish_amd64.deb"
+##URL_SYNOLOGY_DRIVE="https://global.download.synology.com/download/Utility/SynologyDriveClient/3.0.3-12689/Ubuntu/Installer/x86_64/synology-drive-client-12689.x86_64.deb"
+URL_DISCORD="https://discord.com/api/download?platform=linux&format=deb"
+URL_VSCODE="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 
 ##DIRETÓRIOS E ARQUIVOS
 
@@ -38,7 +39,7 @@ SEM_COR='\e[0m'
 # Atualizando repositório e fazendo atualização do sistema
 
 apt_update(){
-  sudo apt update && sudo apt dist-upgrade -y
+    sudo apt update && sudo apt dist-upgrade -y
 }
 
 # -------------------------------------------------------------------------------- #
@@ -59,38 +60,44 @@ fi
 
 ## Removendo travas eventuais do apt ##
 travas_apt(){
-  sudo rm /var/lib/dpkg/lock-frontend
-  sudo rm /var/cache/apt/archives/lock
+    sudo rm /var/lib/dpkg/lock-frontend
+    sudo rm /var/cache/apt/archives/lock
 }
 
 ## Adicionando/Confirmando arquitetura de 32 bits ##
 add_archi386(){
-sudo dpkg --add-architecture i386
+    sudo dpkg --add-architecture i386
 }
+
 ## Atualizando o repositório ##
 just_apt_update(){
-sudo apt update -y
+    sudo apt update -y
 }
 
 ##DEB SOFTWARES TO INSTALL
 
 PROGRAMAS_PARA_INSTALAR=(
-  snapd
-  winff
-  virtualbox
-  ratbagd
-  gparted
-  timeshift
-  gufw
-  synaptic
-  solaar
-  vlc
-  code
-  gnome-sushi 
-  folder-color
-  git
-  wget
-  ubuntu-restricted-extras
+    build-essential
+    snapd
+    tilix
+    postgresql-12
+    zsh
+    ripgrep
+    gparted
+    vlc
+    code
+    git
+    wget
+##    winff
+##    virtualbox
+##    ratbagd
+##    timeshift
+##    gufw
+##    synaptic
+##    solaar
+##    gnome-sushi 
+##    folder-color
+##    ubuntu-restricted-extras
  
 )
 
@@ -100,56 +107,59 @@ PROGRAMAS_PARA_INSTALAR=(
 
 install_debs(){
 
-echo -e "${VERDE}[INFO] - Baixando pacotes .deb${SEM_COR}"
+    echo -e "${VERDE}[INFO] - Baixando pacotes .deb${SEM_COR}"
 
-mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SYNOLOGY_DRIVE"      -P "$DIRETORIO_DOWNLOADS"
+    ##mkdir "$DIRETORIO_DOWNLOADS"
+    ##wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
+    ##wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
+    ##wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
+    ##wget -c "$URL_SYNOLOGY_DRIVE"      -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_DISCORD" -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_VSCODE" -P "$DIRETORIO_DOWNLOADS"
 
-## Instalando pacotes .deb baixados na sessão anterior ##
-echo -e "${VERDE}[INFO] - Instalando pacotes .deb baixados${SEM_COR}"
-sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+    ## Instalando pacotes .deb baixados na sessão anterior ##
+    echo -e "${VERDE}[INFO] - Instalando pacotes .deb baixados${SEM_COR}"
+    sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
 
-# Instalar programas no apt
-echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
+    # Instalar programas no apt
+    echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
 
-for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
-    sudo apt install "$nome_do_programa" -y
-  else
-    echo "[INSTALADO] - $nome_do_programa"
-  fi
-done
+    for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+      if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+        sudo apt install "$nome_do_programa" -y
+      else
+        echo "[INSTALADO] - $nome_do_programa"
+      fi
+    done
 
 }
 ## Instalando pacotes Flatpak ##
 install_flatpaks(){
 
-  echo -e "${VERDE}[INFO] - Instalando pacotes flatpak${SEM_COR}"
+    echo -e "${VERDE}[INFO] - Instalando pacotes flatpak${SEM_COR}"
 
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub org.gimp.GIMP -y
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub com.bitwarden.desktop -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub org.freedesktop.Piper -y
-flatpak install flathub org.chromium.Chromium -y
-flatpak install flathub org.gnome.Boxes -y
-flatpak install flathub org.onlyoffice.desktopeditors -y
-flatpak install flathub org.qbittorrent.qBittorrent -y
-flatpak install flathub org.flameshot.Flameshot -y
-flatpak install flathub org.electrum.electrum -y
+    flatpak install flathub com.obsproject.Studio -y
+    flatpak install flathub org.qbittorrent.qBittorrent -y
+    flatpak install flathub com.spotify.Client -y
+    flatpak install flathub com.bitwarden.desktop -y
+    flatpak install flathub org.telegram.desktop -y
+    ##flatpak install flathub org.gimp.GIMP -y
+    ##flatpak install flathub org.freedesktop.Piper -y
+    ##flatpak install flathub org.chromium.Chromium -y
+    ##flatpak install flathub org.gnome.Boxes -y
+    ##flatpak install flathub org.onlyoffice.desktopeditors -y
+    ##flatpak install flathub org.flameshot.Flameshot -y
+    ##flatpak install flathub org.electrum.electrum -y
+
 }
 
 ## Instalando pacotes Snap ##
 
 install_snaps(){
 
-echo -e "${VERDE}[INFO] - Instalando pacotes snap${SEM_COR}"
+    echo -e "${VERDE}[INFO] - Instalando pacotes snap${SEM_COR}"
 
-sudo snap install authy
+    sudo snap install authy
 
 }
 
@@ -162,40 +172,51 @@ sudo snap install authy
 
 system_clean(){
 
-apt_update -y
-flatpak update -y
-sudo apt autoclean -y
-sudo apt autoremove -y
-nautilus -q
+    apt_update -y
+    flatpak update -y
+    sudo apt autoclean -y
+    sudo apt autoremove -y
+    nautilus -q
 }
 
 
 # -------------------------------------------------------------------------- #
 # ----------------------------- CONFIGS EXTRAS ----------------------------- #
 
+install_neovim(){
+    #Install prerequisites for neovim
+    sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+    git clone https://github.com/neovim/neovim
+    cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo 
+    sudo make install 
+    cd
+    rm -rf ./neovim
+}
+
 #Cria pastas para produtividade no nautilus
 extra_config(){
 
 
-mkdir /home/$USER/TEMP
-mkdir /home/$USER/EDITAR 
-mkdir /home/$USER/Resolve
-mkdir /home/$USER/AppImage
-mkdir /home/$USER/Vídeos/'OBS Rec'
+    mkdir /home/$USER/TEMP
+    mkdir /home/$USER/EDITAR 
+    mkdir /home/$USER/Resolve
+    mkdir /home/$USER/AppImage
+    mkdir /home/$USER/Vídeos/'OBS Rec'
 
 #Adiciona atalhos ao Nautilus
 
-if test -f "$FILE"; then
-    echo "$FILE já existe"
-else
-    echo "$FILE não existe, criando..."
-    touch /home/$USER/.config/gkt-3.0/bookmarks
-fi
+    if test -f "$FILE"; then
+        echo "$FILE já existe"
+    else
+        echo "$FILE não existe, criando..."
+        touch /home/$USER/.config/gkt-3.0/bookmarks
+    fi
 
-echo "file:///home/$USER/EDITAR 🔵 EDITAR" >> $FILE
-echo "file:///home/$USER/AppImage" >> $FILE
-echo "file:///home/$USER/Resolve 🔴 Resolve" >> $FILE
-echo "file:///home/$USER/TEMP 🕖 TEMP" >> $FILE
+    echo "file:///home/$USER/EDITAR 🔵 EDITAR" >> $FILE
+    echo "file:///home/$USER/AppImage" >> $FILE
+    echo "file:///home/$USER/Resolve 🔴 Resolve" >> $FILE
+    echo "file:///home/$USER/TEMP 🕖 TEMP" >> $FILE
+
 }
 
 # -------------------------------------------------------------------------------- #
@@ -210,11 +231,12 @@ add_archi386
 just_apt_update
 install_debs
 install_flatpaks
-install_snaps
-extra_config
+##install_snaps
+##extra_config
 apt_update
 system_clean
+install_neovim
 
 ## finalização
 
-  echo -e "${VERDE}[INFO] - Script finalizado, instalação concluída! :)${SEM_COR}"
+    echo -e "${VERDE}[INFO] - Script finalizado, instalação concluída! :)${SEM_COR}"
